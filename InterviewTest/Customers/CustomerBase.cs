6002be4871f4ac552b020d24e0920482d965172b
pathakHemanta <pathakh@warhawks.ulm.edu>
2023-12-21 12:57:09 -0600

    Add project files.

---
+++ InterviewTest/Customers/CustomerBase.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using InterviewTest.Orders;
+using InterviewTest.Returns;
+
+namespace InterviewTest.Customers
+{
+    public abstract class CustomerBase : ICustomer
+    {
+        private readonly OrderRepository _orderRepository;
+        private readonly ReturnRepository _returnRepository;
+
+        protected CustomerBase(OrderRepository orderRepo, ReturnRepository returnRepo)
+        {
+            _orderRepository = orderRepo;
+            _returnRepository = returnRepo;
+        }
+
+        public abstract string GetName();
+        
+        public void CreateOrder(IOrder order)
+        {
+            _orderRepository.Add(order);
+        }
+
+        public List<IOrder> GetOrders()
+        {
+            return _orderRepository.Get();
+        }
+
+        public void CreateReturn(IReturn rga)
+        {
+            _returnRepository.Add(rga);
+        }
+
+        public List<IReturn> GetReturns()
+        {
+            return _returnRepository.Get();
+        }
+
+        public float GetTotalSales()
+        {
+            float totalSales = 0;
+
+           foreach (var  orders in GetOrders())
+            {
+                foreach(var order in orders.Products)
+                {
+                   totalSales += order.Product.GetSellingPrice();
+                }
+            }
+           
+           //Console.WriteLine($"totalSales: {totalSales}");
+
+           return totalSales;
+           // throw new NotImplementedException();
+        }
+
+        public float GetTotalReturns()
+        {
+            float totalReturns = 0;
+
+            foreach (var returns in GetReturns())
+            {
+                foreach(var eachReturn in returns.ReturnedProducts)
+                {
+                    totalReturns = eachReturn.OrderProduct.Product.GetSellingPrice();
+                }
+            }
+            
+            return totalReturns;
+           // throw new NotImplementedException();
+        }
+
+        public float GetTotalProfit()
+        {
+            float totalProfit = 0;
+
+            totalProfit = GetTotalSales() - GetTotalReturns();
+
+            return totalProfit;
+            //throw new NotImplementedException();
+        }
+    }
+}
---
+++ InterviewTest/Program.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Linq;
+using InterviewTest.Customers;
+using InterviewTest.Orders;
+using InterviewTest.Products;
+using InterviewTest.Returns;
+
+namespace InterviewTest
+{
+    public class Program
+    {
+        private static readonly OrderRepository orderRepo = new OrderRepository();
+        private static readonly ReturnRepository returnRepo = new ReturnRepository();
+
+        static void Main(string[] args)
+        {
+            // ------------------------
+            // Coding Challenge Requirements
+            // ------------------------
+
+
+            // ------------------------
+            // Code Implementations
+            // ------------------------
+            // 1: Implement get total sales, returns, and profit in the CustomerBase class.
+            // 2: Record when an item was purchased.
+
+
+            // ------------------------
+            // Bug fixes
+            // ------------------------
+            // ~~ Run the console app after implementing the Code Changes section above! ~~
+            // 1: Meyer Truck Equipment's returns are not being processed.
+            // 2: Ruxer Ford Lincoln, Inc.'s totals are incorrect.
+            
+
+            // ------------------------
+            // Bonus
+            // ------------------------
+            // 1: Create unit tests for the ordering and return process.
+            // 2: Create a database and refactor all repositories to save/update/pull from it.
+
+            ProcessTruckAccessoriesExample();
+
+            ProcessCarDealershipExample();
+
+            Console.ReadKey();
+        }
+
+        private static void ProcessTruckAccessoriesExample()
+        {
+            var customer = GetTruckAccessoriesCustomer();
+
+            IOrder order = new Order("TruckAccessoriesOrder123", customer);
+            order.AddProduct(new HitchAdapter());
+            order.AddProduct(new BedLiner());
+            customer.CreateOrder(order);
+
+            IReturn rga = new Return("TruckAccessoriesReturn123", order);
+            rga.AddProduct(order.Products.First());
+
+            // Bug Fix
+            // Updates the return repository
+            customer.CreateReturn(rga);
+
+            ConsoleWriteLineResults(customer);
+        }
+
+        private static void ProcessCarDealershipExample()
+        { 
+            var customer = GetCarDealershipCustomer();
+
+            IOrder order = new Order("CarDealerShipOrder123", customer);
+            order.AddProduct(new ReplacementBumper());
+            order.AddProduct(new SyntheticOil());
+            customer.CreateOrder(order);
+
+            IReturn rga = new Return("CarDealerShipReturn123", order);
+            rga.AddProduct(order.Products.First());
+            customer.CreateReturn(rga);
+
+            ConsoleWriteLineResults(customer);
+        }
+
+        private static ICustomer GetTruckAccessoriesCustomer()
+        {
+            return new TruckAccessoriesCustomer(orderRepo, returnRepo);
+        }
+
+        private static ICustomer GetCarDealershipCustomer()
+        {
+            return new CarDealershipCustomer(orderRepo, returnRepo);
+        }
+
+        private static void ConsoleWriteLineResults(ICustomer customer)
+        {
+            Console.WriteLine(customer.GetName());
+
+            Console.WriteLine($"Total Sales: {customer.GetTotalSales().ToString("c")}");
+
+            Console.WriteLine($"Total Returns: {customer.GetTotalReturns().ToString("c")}");
+
+            Console.WriteLine($"Total Profit: {customer.GetTotalProfit().ToString("c")}");
+
+            Console.WriteLine();
+        }
+    }
+}
